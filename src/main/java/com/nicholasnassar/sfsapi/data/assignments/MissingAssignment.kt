3532eaec2a3ac00e48c5e4619fc72046913651c9
Nicholas Nassar <niccholaspage@gmail.com>
2017-05-26 01:21:08 -0400

    Fix MissingAssignment non-null parameters

--- src/main/java/com/nicholasnassar/sfsapi/data/assignments/MissingAssignment.kt
+++ src/main/java/com/nicholasnassar/sfsapi/data/assignments/MissingAssignment.kt
@@ -3,8 +3,8 @@ package com.nicholasnassar.sfsapi.data.assignments
 import com.nicholasnassar.sfsapi.data.links.Link
 import io.vertx.core.json.JsonObject
 
-class MissingAssignment(val link: Link, val dateDue: String, val clazz: String, val assignment: String,
-                        val possiblePoints: String, val score: String, val comments: String) {
+class MissingAssignment(val link: Link, val dateDue: String?, val clazz: String?, val assignment: String?,
+                        val possiblePoints: String?, val score: String?, val comments: String?) {
 
     fun asJson(): JsonObject {
         return JsonObject().put("link", link.asJson()).put("date_due", dateDue).put("class", clazz)
