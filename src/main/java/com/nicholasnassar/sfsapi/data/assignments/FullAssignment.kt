19c53352cd8380686eb9061fc433944c715d5e04
Nicholas Nassar <niccholaspage@gmail.com>
2017-05-28 21:53:19 -0400

    Remove blank lines in FullAssignment and MissingAssignment

--- src/main/java/com/nicholasnassar/sfsapi/data/assignments/FullAssignment.kt
+++ src/main/java/com/nicholasnassar/sfsapi/data/assignments/FullAssignment.kt
@@ -6,7 +6,6 @@ import io.vertx.core.json.JsonObject
 class FullAssignment(val activity: String, val clazz: String?, val instructor: String?, val assigned: String?,
                      val due: String?, val possiblePoints: String?, val category: String?, val notes: String?,
                      val resources: List<Resource>) {
-
     fun asJson(): JsonObject {
         return JsonObject().put("activity", activity).put("class", clazz).put("instructor", instructor)
                 .put("assigned", assigned).put("due", due).put("possible_points", possiblePoints)
--- src/main/java/com/nicholasnassar/sfsapi/data/assignments/MissingAssignment.kt
+++ src/main/java/com/nicholasnassar/sfsapi/data/assignments/MissingAssignment.kt
@@ -5,7 +5,6 @@ import io.vertx.core.json.JsonObject
 
 class MissingAssignment(val link: Link, val dateDue: String?, val clazz: String?, val assignment: String?,
                         val possiblePoints: String?, val score: String?, val comments: String?) {
-
     fun asJson(): JsonObject {
         return JsonObject().put("link", link.asJson()).put("date_due", dateDue).put("class", clazz)
                 .put("assignment", assignment).put("possible_points", possiblePoints).put("score", score)
